8ed3547869f65479a614dca8c364b0f88e8bd516
ritu raj <rituraj8919@gmail.com>
2025-06-25 17:41:44 +0530

    updated first section

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}", "./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2786a5",
+      },
+    },
+  },
+  plugins: [],
+};
